bdbb9e7803618849fef3163a546554e6ec57bc5b
Anatolii <gumby@ukr.net>
2018-07-02 14:29:58 +0300

    initial

---
+++ tcp-server/index.js
@@ -0,0 +1,36 @@
+/*
+In the node.js intro tutorial (http://nodejs.org/), they show a basic tcp 
+server, but for some reason omit a client connecting to it.  I added an 
+example at the bottom.
+Save the following server in example.js:
+*/
+
+var net = require('net');
+
+var server = net.createServer(function(socket) {
+
+	socket.setEncoding('utf8');
+
+	socket.on('error', function(err) {
+   		console.log(err)
+	});
+
+	socket.on('data', function(data) {
+		console.log(data);
+		socket.write(data);
+	});
+
+	socket.write('Echo server\r\n');
+	//socket.pipe(socket);
+});
+
+server.listen(1337, '127.0.0.1');
+
+/*
+And connect with a tcp client from the command line using netcat, the *nix 
+utility for reading and writing across tcp/udp network connections.  I've only 
+used it for debugging myself.
+$ netcat 127.0.0.1 1337
+You should see:
+> Echo server
+*/
