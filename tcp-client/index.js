f7ebe08cb1e6a1eb201b2a7493c214efc0daf70a
Anatolii <gumby@ukr.net>
2018-07-04 12:01:39 +0300

    stacking data to db

--- tcp-client/index.js
+++ tcp-client/index.js
@@ -1,20 +1,50 @@
-/* Or use this example tcp client written in node.js.  (Originated with 
-example code from 
-http://www.hacksparrow.com/tcp-socket-programming-in-node-js.html.) */
 
 var net = require('net');
 
+var velocity = 0;
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	console.log('MATH NUMBER: ',Math.random());
+	return Math.floor(Math.random() * (max - min)) + min; //Включно з мінімальним та виключаючи максимальне значення 
+}
+
+function getInc(val, min, max) {
+	val += 1;
+	if(val >= max){
+		val = min;
+	}
+  	return val; 
+}
+
 var client = new net.Socket();
+
+function myConnect(message){
+	client.connect(1337, '127.0.0.1', function() {
+		console.log('Connected');
+		client.write(message);
+	});
+}
+
 client.connect(1337, '127.0.0.1', function() {
 	console.log('Connected');
-	client.write('Hello, server! Love, Client.');
+	setInterval(function() {
+
+					client.write('(027043887991BR00160718A4928.5660N03159.8943E000.0035519264.2000000000L00000000)');
+
+				},2000);
 });
 
 client.on('data', function(data) {
 	console.log('Received: ' + data);
-	client.destroy(); // kill client after server's response
+	//client.destroy(); // kill client after server's response
 });
 
 client.on('close', function() {
 	console.log('Connection closed');
+});
+
+client.on('error', function(err) {
+		console.log(err)
 });
--- tcp-server/index.js
+++ tcp-server/index.js
@@ -1,11 +1,29 @@
-/*
-In the node.js intro tutorial (http://nodejs.org/), they show a basic tcp 
-server, but for some reason omit a client connecting to it.  I added an 
-example at the bottom.
-Save the following server in example.js:
-*/
 
 var net = require('net');
+
+var Sequelize = require('sequelize');
+
+var connection = new Sequelize('tracker_db', 'track-user', 'password',{
+	host: 'localhost',
+  	dialect: 'mysql',
+  	operatorsAliases: false,
+
+  	pool: {
+    	max: 5,
+    	min: 0,
+    	acquire: 30000,
+    	idle: 10000
+  	}
+});
+
+var Article = 	connection.define('track_data', {
+	date: Sequelize.INTEGER,
+	lat: Sequelize.FLOAT(8,6),
+	lng: Sequelize.FLOAT(9,6),
+	speed: Sequelize.FLOAT(4,1),
+	direction: Sequelize.FLOAT(5,2),
+	gpsUid: Sequelize.STRING(12)
+});
 
 var server = net.createServer(function(socket) {
 
@@ -16,7 +34,71 @@ var server = net.createServer(function(socket) {
 	});
 
 	socket.on('data', function(data) {
-		console.log(data);
+		//console.log(data);
+		var myRegExp = new RegExp(/\(([0-9]+)(BR00)([^A-Z]+)(A)([0-9]{2})([0-9\.]+)([NS])([0-9]{3})([0-9\.]+)([EW])([0-9\.]{5})[0-9]{6}([0-9\.]{6})[0-9]+([L])([0-9]+)\)\s*\r*\n*/);
+		var execRes = myRegExp.exec(data);
+
+		// for data:
+		// 		(027042573071BR00141206A1904.6176N07250.8073E002.50952072.730000000000L00000000)
+		// dataArray should contain:
+		//	[	0:	"(027042573071BR00141206A1904.6176N07250.8073E002.50952072.730000000000L00000000)",
+		// 		1:	"027042573071",			=> imei number (should have been only 10 chars, but its 12)
+		// 		2:	"BR00",					=> command word
+		// 		3:	"141206",				=> date YYMMDD
+		// 		4:	"A",					=> available
+		// 		5:	"19", 					=> longitude degrees (0-90 only)
+		// 		6:	"04.6176", 				=> longitude minutes
+		// 		7:	"N",					=> longitude hemisphere
+		// 		8:	"072", 					=> latitude degrees (0-180 only)
+		// 		9:	"50.8073", 				=> latitude minutes
+		// 		10:	"E", 					=> latitude E/W
+		// 		11:	"002.5",	 			=> speed (knot/hr or km/hr)
+		// 		12:	"072.7",				=> direction in degrees. 0=north, 90=east, 180=south, 270=west
+		// 		13:	"L",					=> 'L' means Total mileage, unit is meter, mileage statistic
+		// 		14:	"00000000",				=> 
+		// 		15:	""
+		// 	]
+
+		var lat = (parseInt(execRes[5]) + parseFloat(execRes[6])/60) * ( (execRes[7] === "N") ? 1 : -1 );
+		var lng = (parseInt(execRes[8]) + parseFloat(execRes[9])/60) * ( (execRes[10] === "E") ? 1 : -1 );
+		var date = execRes[3];//.split(/([0-9]{2})([0-9]{2})([0-9]{2})/);
+		var ctime = (new Date(date[2] + "-" + date[3] + "-" + date[1] )).getTime();
+		var gpsUid = execRes[1];
+		var speed = execRes[11];
+		var direction = execRes[12];
+
+		console.log('latitude: ', lat,
+					'\r\nlongitude: ', lng,
+					'\r\ndate: ', date,
+					'\r\nctime: ', ctime,
+					'\r\ngpsUid: ', gpsUid,
+					'\r\nspeed: ', speed,
+					'\r\ndirection: ', direction
+		);
+
+		connection.sync().then(function (){
+			Article.create({
+				date: date,
+				lat: lat,
+				lng: lng,
+				speed: speed,
+				direction: direction,
+				gpsUid: gpsUid
+			});
+		});
+ 
+
+	//"gpsUid=%s&msgType=locationUpdate&loc=[%s,%s]&speed=%s&direction=%s",
+	// deviceId, latitude, longitude, speed, direction)
+	// var httpRes =
+	// 	  'gpsUid=' + execRes[1]
+	// 	+ '&msgType=locationUpdate'
+	// 	+ '&loc=['+ lat+','+ lng+']'
+	// 	+ '&speed='+ execRes[11]
+	// 	+ '&direction='+ execRes[12]
+	// 	+ '&ctime='+ ctime;
+	//console.log("gt02a:BR00: httpRes is: ", httpRes);
+		
 		socket.write(data);
 	});
 
